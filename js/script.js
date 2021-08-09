ab64aac7a718498dc2f545bfff45c129069f7768
Jason Chua <git@jasonchua.net>
2021-08-08 19:39:49 -0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* script.js 
+   Author:
+   Date:
+*/
+
+
+$(document).ready(function(){ // begin document.ready block
+
+	//Used for the photo grid
+	$(".gridpic").hover(function(){
+		$('.photocaption', this).fadeToggle();
+
+	});
+
+}); //end document.ready block
